8fbd49d0afac22f21041fad007d4c0c86ce00986
einaros <einaros@gmail.com>
2012-12-02 00:58:52 +0100

    added commands for grab script

--- plugins/grab.js
+++ plugins/grab.js
@@ -15,33 +15,63 @@ function ensurePathExists(root, targetPath, callback) {
   }).then(callback);
 }
 
+var headers = {};
+
+var api = {
+  'set-grab-header': {
+    usage: 'set-grab-header <headername> [value]',
+    handler: function(logger, callback, headername, value) {
+      if (!headername) {
+        logger.warn('Usage: ', api['set-grab-header'].usage);
+        return callback();
+      }
+
+      if (!value) {
+        logger.info('Unset header "%s" for "grab" api.', [headername]);
+        delete headers[headername];
+      }
+      else {
+        logger.info('Set header "%s" to "%s" for "grab" api.', [headername, value]);
+        headers[headername] = value;
+      }
+    }
+  },
+  'get-grab-headers': {
+    usage: 'get-grab-headers',
+    handler: function(logger, callback, headername, value) {
+      logger.info('Headers: %s', [JSON.stringify(headers, null, 2)]);
+    }
+  },
+  'grab': {
+    usage: 'grab <url> [outputpath]',
+    handler: function(logger, callback, fetchUrl, output) {
+      if (!fetchUrl) {
+        logger.warn('Usage: ', api.grab.usage);
+        return callback();
+      }
+
+      logger.info('Downloading "%s".', [fetchUrl]);
+      var parsedUrl = url.parse(fetchUrl);
+      request({ headers: headers, url: fetchUrl }, function (error, response, body) {
+        if (error) return callback(error);
+        if (response.statusCode != 200) return callback(new Error('Server returned code: ' + response.statusCode));
+        var targetPath = path.join(process.cwd(), parsedUrl.hostname, parsedUrl.pathname).trim();
+        if (targetPath[targetPath.length - 1] == '/') targetPath += 'index.html';
+        if (output) targetPath = path.join(path.dirname(targetPath), output);
+        logger.info('Saving as "%s".', [targetPath]);
+        ensurePathExists(process.cwd(), path.dirname(targetPath), function(error) {
+          if (error) return callback(error);
+          fs.writeFile(targetPath, body, callback);
+        });
+      });
+    }
+  }
+};
+
 module.exports = {
   name: 'Script command: grab',
 
   getScriptApi: function() {
-    return {
-      grab: {
-        usage: 'grab <url> [outputpath]',
-        handler: function(logger, callback, fetchUrl, output) {
-          if (!fetchUrl) {
-            logger.warn('Usage: fetch <url> [filename]');
-          }
-          else {
-            var parsedUrl = url.parse(fetchUrl);
-            request(fetchUrl, function (error, response, body) {
-              if (error) return callback(error);
-              if (response.statusCode != 200) return callback(new Error('Server returned code: ' + response.statusCode));
-              var targetPath = path.join(process.cwd(), parsedUrl.hostname, parsedUrl.pathname).trim();
-              if (targetPath[targetPath.length - 1] == '/') targetPath += 'index.html';
-              if (output) targetPath = path.join(path.dirname(targetPath), output);
-              ensurePathExists(process.cwd(), path.dirname(targetPath), function(error) {
-                if (error) return callback(error);
-                fs.writeFile(targetPath, body, callback);
-              });
-            });
-          }
-        }
-      }
-    }
+    return api;
   }
 };
